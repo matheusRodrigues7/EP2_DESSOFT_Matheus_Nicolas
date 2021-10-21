fbdbb8963dc6817c2d266d788b08f35585eee2b1
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-10-21 20:24:33 -0300

    Introdução

--- dev1.py
+++ dev1.py
@@ -1,3 +1,11 @@
-print('Matheus')
+print('Jogo de Dominó')
+print('Por Matheus e Nicolas')
+print('=> Design de Software')
 
-print('1berto')
+print('Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar sem peças na sua mão antes dos outros jogadores.')
+print('Vamos começar!!!')
+
+n_jogadores = int(input('Quantos jogadores? (2-4)'))
+
+while n_jogadores > 4 or  n_jogadores < 2:
+    n_jogadores = int(input('Quantos jogadores? (2-4)'))
