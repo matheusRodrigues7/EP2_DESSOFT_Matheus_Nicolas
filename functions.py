70c3bfeb96d18da6ce8b31349d21012fcaba0d66
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-10-22 19:42:36 -0300

    Funções

--- functions.py
+++ functions.py
@@ -1,15 +1,11 @@
 def verifica_ganhador(jogadores):
-
     for jogador,pecas in jogadores.items():
         if len(pecas) == 0:
             return jogador
-    
     return -1
 
 def soma_pecas(pecas):
-
     soma = 0
     for peca in pecas:
-        soma += sum(peca)
-        
+        soma += sum(peca) 
     return soma
