28d663b68fc4028ff53657161944d8309098711e
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-10-25 18:10:35 -0300

    comeco-jogo

--- Jogo.py
+++ Jogo.py
@@ -1,1 +1,12 @@
+print('Jogo de Dominó')
+print('Por Matheus e Nicolas')
+print('=> Design de Software')
 
+print('Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar sem peças na sua mão antes dos outros jogadores.')
+print('Vamos começar!!!')
+
+n_jogadores = int(input('Quantos jogadores? (2-4) '))
+
+while n_jogadores > 4 or n_jogadores < 2:
+    print('Número inválido!')
+    n_jogadores = int(input('Quantos jogadores? (2-4)'))
