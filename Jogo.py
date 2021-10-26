e39c744ce874f392bc15a32bc577cbdb190fb876
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-10-26 18:55:11 -0300

    Arrumando bug

--- Jogo.py
+++ Jogo.py
@@ -17,19 +17,23 @@ while n_jogadores > 4 or n_jogadores < 2:
     print('Número inválido!')
     n_jogadores = int(input('Quantos jogadores? (2-4)'))
 
+pecas = functions.cria_pecas()
+call = functions.inicia_jogo(n_jogadores,pecas)
+mesa = call['mesa']
+
 indice_1j = random.randint(0,n_jogadores-1) #Indice do primeiro jogador.
 if indice_1j == 0:
     jogador_inicia = 'Você'
 else:
     jogador_inicia=indice_1j
 
-pecas = functions.cria_pecas()
-call = functions.inicia_jogo(n_jogadores,pecas)
-mesa = call['mesa']
+
+
 while indice_1j <= n_jogadores:
-    indice_1j+=1
-    if indice_1j==n_jogadores:
-        indice_1j=0
+    
+    if indice_1j == n_jogadores:
+        indice_1j = 0
+
     if len(mesa) == 0:
         print('MESA: \n')
     else:
@@ -43,30 +47,36 @@ while indice_1j <= n_jogadores:
 
     possiveis = functions.posicoes_possiveis(mesa,pecas_jogador)
 
-    possiveis1 = []
-    for indice in possiveis: 
-        possiveis1.append(indice + 1)
-
     peca = pecas_jogador[possiveis[0]]
+    
+    if indice_1j == 0:
+        possiveis1 = []
+        for indice in possiveis: 
+            possiveis1.append(indice + 1)
 
-    if jogador_inicia == 'Você':
-        print(possiveis1)
+        print (f'Jogador: Você com {qtde_pecas} peça(s)')
         print(pecas_jogador)
+        print(possiveis1)
         escolha_peca = int(input('Escolha a peça: '))
 
         while escolha_peca not in possiveis1:
             print('Posição inválida!')
             escolha_peca = int(input(f'Escolha a peça {possiveis1} '))
-
-        peca = pecas_jogador[possiveis[escolha_peca-1]]
+        
+        peca = pecas_jogador[escolha_peca-1]
+    
+    else:
+        print(f'Jogador: {indice_1j+1} com {qtde_pecas} peça(s)')
+    
+    print(f'Colocou: {peca}')
 
     pecas_jogador.remove(peca)
-    print (f'Jogador: {jogador_inicia} com {qtde_pecas} peça(s)')
-    print(f'Colocou: {peca}')
 
     # redefinindo mesa
     mesa = functions.adiciona_na_mesa(peca,mesa)
-    print(f'MESA: \n{mesa}')
+    '''print(f'MESA: \n{mesa}')'''
 
-    print(call['monte'])
-    print (call['jogadores'])
+    indice_1j += 1
+
+    '''print(call['monte'])
+    print (call['jogadores'])'''
