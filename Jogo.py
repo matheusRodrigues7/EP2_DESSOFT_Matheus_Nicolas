09c6b45be1024d6d0ea8825569a5b5f906d135b3
Matheus Rodrigues <matheussr@al.insper.edu.br>
2021-10-26 08:44:30 -0300

    mais algumas funcionalidades

--- Jogo.py
+++ Jogo.py
@@ -17,22 +17,39 @@ while n_jogadores > 4 or n_jogadores < 2:
     print('Número inválido!')
     n_jogadores = int(input('Quantos jogadores? (2-4)'))
 
-indice_1j=random.randint(0,n_jogadores-1) #Indice do primeiro jogador.
-if indice_1j==0:
-    jogador_inicia='Você'
+indice_1j = random.randint(0,n_jogadores-1) #Indice do primeiro jogador.
+if indice_1j == 0:
+    jogador_inicia = 'Você'
 else:
     jogador_inicia=indice_1j
 
-pecas=functions.cria_pecas()
-call=functions.inicia_jogo(n_jogadores,pecas)
-mesa= call['mesa']
-while algo:
-    quantidade_pecas= len(call['jogadores'][indice_1j])
-    print (f'MESA:{mesa}')
+pecas = functions.cria_pecas()
+call = functions.inicia_jogo(n_jogadores,pecas)
+mesa = call['mesa']
+#while algo:
+if len(mesa) == 0:
+    print('MESA: \n')
+else:
+    print(f'MESA: \n {mesa}')
+quantidade_pecas = len(call['jogadores'][indice_1j])
 
-    print (f'Jogador: {jogador_inicia} com {quantidade_pecas} peças')
 
+# Chamar a função add peças na mesa
+# Chamar a função posições possíveis
 
+possiveis = functions.posicoes_possiveis(mesa,call['jogadores'][indice_1j])
 
-    print(call['monte'])
-    print (call['jogadores'])
+possiveis1 = []
+for indice in possiveis: 
+    possiveis1.append(indice + 1)
+
+print (f'Jogador: {jogador_inicia} com {quantidade_pecas} peça(s)')
+
+peca = int(input('Escolha a peça: '))
+
+while peca not in possiveis1:
+    print('Posição inválida!')
+    peca = int(input(f'Escolha a peça [{possiveis1}] '))
+
+print(call['monte'])
+print (call['jogadores'])
