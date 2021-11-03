ece0a9fb59442dfca0b5e88e4cc5f335b023c51e
Nicolas Yassuda <nicolasey@al.insper.edu.br>
2021-11-03 18:32:21 -0300

    Tratamento de Erro

--- Jogo_corrigido.py
+++ Jogo_corrigido.py
@@ -11,12 +11,19 @@ print('=> Design de Software')
 print('Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar sem peças na sua mão antes dos outros jogadores.')
 print('Vamos começar!!!')
 continuar= 'S'
+Tratamento = True
+Tratamento_possiveis = True
 while continuar == 'S' or continuar == 's':
-    n_jogadores = int(input('Quantos jogadores? (2-4) '))
-
-    while n_jogadores < 2 or n_jogadores > 4:
-        print('Número inválido!')
-        n_jogadores = int(input('Quantos jogadores? (2-4)'))
+    while Tratamento or (n_jogadores < 2 or n_jogadores > 4):
+        n_jogadores=2
+        try:
+            n_jogadores = int(input('Quantos jogadores? (2-4) '))
+            Tratamento = False
+        except ValueError:
+            print('Digito inválido')
+            Tratamento = True
+        if n_jogadores < 2 or n_jogadores > 4:
+            print('Digito inválido')
 
     pecas = functions.cria_pecas()
     call = functions.inicia_jogo(n_jogadores,pecas)
@@ -26,19 +33,10 @@ while continuar == 'S' or continuar == 's':
     soma = functions.cria_dic_soma(n_jogadores)
     passe=0
 
-    #mesa_anterior=[]
-    #mesa_posterior=[]
-
     indice_1j = random.randint(0,n_jogadores-1) #Sorteando o primeiro jogador
-
-    '''if indice_1j == 0:
-        jogador_inicia = 'Você'
-    else:
-        jogador_inicia = indice_1j'''
 
     while indice_1j <= n_jogadores:
 
-        #print(passe,'INICIO')
         if indice_1j == n_jogadores:
             indice_1j = 0
 
@@ -49,17 +47,6 @@ while continuar == 'S' or continuar == 's':
         possiveis = functions.posicoes_possiveis(mesa,pecas_jogador)
 
         nao_tem_peca = True
-        '''if len(possiveis) == 0:
-            if n_jogadores == 4:
-                print('Não tem peças possíveis. MONTE VAZIO - PULANDO A VEZ!')
-                passe += 1
-                print('Passe: ', passe)
-                indice_1j += 1
-                if passe == n_jogadores:
-                    break
-                continue
-                
-            else:'''
         while nao_tem_peca:
             if len(possiveis) == 0 and len(monte) == 0:
                 break
@@ -74,26 +61,17 @@ while continuar == 'S' or continuar == 's':
                         
                     print('Não tem peças possíveis. PEGANDO DO MONTE!')
                     print(f'\nMESA: \n{functions.print_mesa(mesa)}\n')
-                    #print(f'MESA: \n{mesa}')
+
                     pecas_jogador.append(monte[0])
                     monte.remove(monte[0])
                     possiveis = functions.posicoes_possiveis(mesa,pecas_jogador)
-                    #print(monte,'CHECAGEM') #Checagem
             else:
                 nao_tem_peca = False
-            #print(pecas_jogador)
         
         qtde_pecas = len(pecas_jogador)
 
         if len(possiveis) > 0:
             peca = pecas_jogador[possiveis[0]]
-        '''elif len(possiveis) == 0 and len(monte) == 0:
-            print(f'{indice_1j} passou')
-            passe += 1
-            if passe < 4: 
-                continue
-            else: 
-                break'''
 
         if indice_1j == 0:
             possiveis1 = []
@@ -102,14 +80,20 @@ while continuar == 'S' or continuar == 's':
                 
             print (f'Jogador: Você com {qtde_pecas} peça(s)')
             print(functions.print_pecas(pecas_jogador))
+            
             if len(possiveis) > 0:
                 print(f'Peças possíveis: {possiveis1}')
-                escolha_peca = int(input('Escolha a peça: '))
-
-                while escolha_peca not in possiveis1:
-                    print('Posição inválida!')
-                    escolha_peca = int(input(f'Escolha a peça {possiveis1} '))
-                
+                while Tratamento_possiveis or escolha_peca not in possiveis1:
+                    escolha_peca = possiveis1[0]
+                    try:
+                        escolha_peca = int(input('Escolha a peça: '))
+                        Tratamento_possiveis = False
+                    except ValueError:
+                        print('Posição inválida!')
+                        Tratamento_possiveis=True
+                    if escolha_peca not in possiveis1:
+                        print('Posição inválida!')
+                    print(f'Peças possíveis: {possiveis1}')
                 peca = pecas_jogador[escolha_peca-1]
         
         else:
@@ -133,11 +117,9 @@ while continuar == 'S' or continuar == 's':
         mesa_anterior = mesa[:]
         mesa = functions.adiciona_na_mesa(peca,mesa)
         mesa_posterior = mesa[:]
-        '''print(f'MESA: \n{mesa}')'''
         
         if len(mesa_anterior) < len(mesa_posterior):
             passe = 0
-            #print(passe,'NÃO PASSOU')
 
         if len(pecas_jogador) == 0:
             break
@@ -148,7 +130,6 @@ while continuar == 'S' or continuar == 's':
     for i in range (n_jogadores):
         pontos = functions.soma_pecas(jogadores[i])
         soma[i] += pontos
-    #print(soma)
 
     vencedor = functions.verifica_ganhador(jogadores)
     
